f92b59fdcf70cbab189c042e898588fd5df1e323
Carlos Vargas <varlopecar@gmail.com>
2025-03-21 10:28:04 +0100

    chore: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vitest-vite-app/',
   plugins: [react()],
   build: {
     outDir: 'docs'
